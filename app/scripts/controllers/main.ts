5524c040ded748de1617f1d432aabf7e28ee2bd1
ElodieBoy <elodie_boy@hotmail.fr>
2016-04-12 17:53:13 +0200

    Ajout du model, modif formulaire d'ajout notapaf

--- app/scripts/controllers/main.ts
+++ app/scripts/controllers/main.ts
@@ -1,18 +1,26 @@
-/// <reference path="../app.ts" />
+/// <reference path="../model/notapaf.ts" />
 
 'use strict';
 
 module appApp {
   export interface IMainScope extends ng.IScope {
     tags: any[];
+    notapaf: model.INotapaf;
   }
 
   export class MainCtrl {
 
     public static $inject: string[] = ['$scope'];
+    public scope: IMainScope;
 
     constructor (private $scope: IMainScope) {
       $scope.tags = [];
+      this.scope = $scope;
+    }
+
+    private save: any = () => {
+      console.log("test");
+      console.log(this.scope.notapaf);
     }
   }
 }
