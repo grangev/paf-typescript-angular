3b141647c202eba7da4bd221d729f6fdffe9b9ff
benwayne182 <benwayne182@gmail.com>
2016-04-13 19:39:57 +0200

    liaison vues 1 et 3

--- app/scripts/controllers/main.ts
+++ app/scripts/controllers/main.ts
@@ -7,6 +7,7 @@
 module appApp {
   import IHttpService = angular.IHttpService;
   import INotapaf = model.INotapaf;
+  import ILocationService = angular.ILocationService;
   export interface IMainScope extends ng.IScope {
     notapaf: model.INotapaf;
     notapafForm: any;
@@ -14,13 +15,13 @@ module appApp {
 
   export class MainCtrl {
 
-    public static $inject = ['$scope', '$http', 'getUrl'];
+    public static $inject = ['$scope', '$http', 'getUrl', '$location'];
     public scope:IMainScope;
     public service:Geturl;
     public  dataUrl : model.INotapaf[];
 
 
-    constructor(private $scope:IMainScope, private $http : IHttpService, private getUrl : Geturl) {
+    constructor(private $scope:IMainScope, private $http : IHttpService, private getUrl : Geturl, private $location: ILocationService) {
       this.scope = $scope;
       this.service=getUrl;
       this.service.getdataurl().then((data)=> {
@@ -36,6 +37,12 @@ module appApp {
       $('#formUrl').modal('hide');
       var form: any = $('#notapafParent').find('form')[0];
       form.reset();
+    }
+
+    private go: any =(url : string) => {
+      this.$location.path('/formulaire/'+url);
+
+
     }
   }
 }
